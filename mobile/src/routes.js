c06c581c3ce3e04da1dd25c6ee68579b5b2a1a2c
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-03-26 17:06:53 -0300

    Hiding headers

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator  } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
@@ -8,13 +8,13 @@ import Detail from './pages/Detail';
 import Incidents from './pages/Incidents';
 
 export default function Routes() {
-    
+    // screenOptins={{ headerShown: false }}
     return (
         <NavigationContainer>
-            <AppStack.Navigator screenOptins={{ headerShown: false }}>
-                <AppStack.Screen name="Incidents" component={Incidents} />
-                <AppStack.Screen name="Detail" component={Detail} />
+            <AppStack.Navigator>
+                <AppStack.Screen name="Incidents" component={Incidents} options={{ headerShown: false }} />
+                <AppStack.Screen name="Detail" component={Detail} options={{ headerShown: false }}/>
             </AppStack.Navigator>
         </NavigationContainer>
-    );  
+    );
 }
